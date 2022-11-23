c229ae953b17e1212d15ae5512c308005eb13976
KerimliVusal <kerimlivusal98@gmail.com>
2022-11-23 12:54:54 +0400

    last

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux"; 
+import configureStore from './redux/redux';
+import { BrowserRouter } from "react-router-dom";
+
 
 const store = configureStore();
 ReactDOM.render(
